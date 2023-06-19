647cf64495f4bb970d36cd82fe580399e6b64070
danben1 <124446045+danben1@users.noreply.github.com>
2023-06-19 09:47:05 +0100

    wknd dev complete

--- frontend/src/components/ListItem/index.js
+++ frontend/src/components/ListItem/index.js
@@ -1,34 +1,27 @@
-import React from 'react';
-import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
-import RestaurantIcon from '@mui/icons-material/Restaurant';
-import './styles.css';
+import React from "react";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import "./styles.css";
 
-function ListItem ({title}) {
-
+function ListItem({ title }) {
     const iconStyle = {
-        color: 'white'
-    }
+        color: "white",
+    };
     return (
-        <div className='listItem' theme xs={4} sm={5} md={7} lg={9} xl={11}>
-            <div className='listItem__icons'>
-                <div className='listItem__icons--activity'>
+        <div className="listItem" theme xs={4} sm={5} md={7} lg={9} xl={11}>
+            <div className="listItem__icons">
+                <div className="listItem__icons--activity">
                     <RestaurantIcon style={iconStyle} />
                 </div>
-                <div className='listItem__icons--favourite'>
+                <div className="listItem__icons--favourite">
                     <BookmarkBorderOutlinedIcon style={iconStyle} />
                 </div>
             </div>
-            <div className='listItem__title'>
+            <div className="listItem__title">
                 <h3>{title}</h3>
             </div>
-
-
-        </div>    
-        
-        )
+        </div>
+    );
 }
 
 export default ListItem;
-
-
-
